0b3af8963e2446bc0ec1302cd22b7fb4fede22d7
LuisAdriano014 <106563197+LuisAdriano014@users.noreply.github.com>
2022-09-22 16:49:39 -0300

    Atualizada

--- Questão02.java
+++ Questão02.java
@@ -4,33 +4,63 @@ import java.util.Scanner;
 
 public class Questão02 {
 	
-	public static int Valor (String dia, int valor) {
+	public static int Valor (String dia, String tipo, int valor) {
 		
 		int resultado = 0; 
-
-		switch (dia.charAt(2)) {
 		
-		case 'g':
-			resultado = (int) (valor - 0.4*valor);
-			break;
-		case 'r':
-			resultado = (int) (valor - 0.4*valor);
-			break;
-		case 'a':
-			resultado = valor;
-			break;
-		case 'i':
-			resultado = valor;
-			break;
-		case 'x':
-			resultado = valor;
-			break;
-		case 'b':
-			resultado = valor;
-			break;
-		case 'm':
-			resultado = valor;
-			break;
+		if (tipo.charAt(2) == 'm') {
+			switch (dia.charAt(2)) {
+		
+			case 'g':
+				resultado = (int) (valor - 0.4*valor);
+				break;
+			case 'r':
+				resultado = (int) (valor - 0.4*valor);
+				break;
+			case 'a':
+				resultado = valor;
+				break;
+			case 'i':
+				resultado = valor;
+				break;
+			case 'x':
+				resultado = valor;
+				break;
+			case 'b':
+				resultado = valor;
+				break;
+			case 'm':
+				resultado = valor;
+				break;
+			}
+		}else if (tipo.charAt(2) == 'v') {
+			
+			int valor2 = (int) (1.16*valor);
+			
+			switch (dia.charAt(2)) {
+		
+			case 'g':
+				resultado = (int) (valor2 - 0.4*valor2);
+				break;
+			case 'r':
+				resultado = (int) (valor2 - 0.4*1.15*valor2);
+				break;
+			case 'a':
+				resultado = valor2;
+				break;
+			case 'i':
+				resultado = valor2;
+				break;
+			case 'x':
+				resultado = valor2;
+				break;
+			case 'b':
+				resultado = valor2;
+				break;
+			case 'm':
+				resultado = valor2;
+				break;
+			}
 		}
 		
 		return resultado;
@@ -44,9 +74,12 @@ public class Questão02 {
 		System.out.println("Digite o dia da semana");
 		String dia = string.nextLine();
 		
+		System.out.println("A ferramentas são comuns ou novas?");
+		String tipo = string.nextLine();
+		
 		System.out.println("Digite o valor da ferramenta");
 		int valor = ferramenta.nextInt(); 
 		
-		System.out.println("Resultado: "+ Valor (dia, valor));
+		System.out.println("Resultado: "+ Valor (dia, tipo, valor));
 	}
 }
